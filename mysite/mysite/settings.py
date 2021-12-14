4544435c971f4c12a8f9c63ec58980c657d5bdb9
PazNicolas <nicopa@outlook.com>
2021-12-14 00:57:06 -0300

    Comentarios

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'g6db',
-        'USER': 'grupo6',
-        'PASSWORD': 'holagrupo6',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'g6db',
+       'USER': 'grupo6',
+       'PASSWORD': 'holagrupo6',
+       'HOST': '127.0.0.1',
+       'PORT': '3306',
 
-    }
+  }
 }
 
 
