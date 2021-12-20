5c2ac1dddf259a2e5200876f52c8fb3eb9c9917a
PazNicolas <nicopa@outlook.com>
2021-12-20 18:18:27 -0300

    media

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -133,9 +133,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIR = (
-    os.path.join(BASE_DIR, 'static')
-)
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'))
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
